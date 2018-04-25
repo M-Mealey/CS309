fb4b9b3f1366f726e3a2472fabdd8470f70d667d
Megan Mealey <mealeymn@gmail.com>
2018-04-25 13:47:07 -0500

    moved extra data sets to a folder

--- read.py
+++ read.py
@@ -12,20 +12,19 @@ def tsToNum(s):
     minute = s[14:16]
     sec = s[17:19]
     time = float(hour)+float(minute)*(1.0/60)+float(sec)*(1.0/3600)
-    return time
+    return 60.0*time
 
 # read call numbers
 call_nums=np.genfromtxt(data_name, dtype=long, delimiter=",",skip_header=1, usecols=0)
 # gets index of duplicate call numbers
 # this just keeps the first one and deletes the rest, we should
 # think about how to determine which to keep
-print call_nums
-ret0,ret1 = np.unique(call_nums,return_index=True)
-unique = np.isin(range(0,20),ret1)
-for i in range(0,unique.size):
-    if not unique[i]:
-        bad_ind.append(i)
-print call_nums
+#ret0,ret1 = np.unique(call_nums,return_index=True)
+#unique = np.isin(range(0,20),ret1)
+#for i in range(0,unique.size):
+#    if not unique[i]:
+#        bad_ind.append(i)
+#print call_nums
 
 
 # read call types (as strings)
@@ -85,7 +84,6 @@ time_nums = []
 for i in range(0,call_in_ts.size):
     time_nums.append(tsToNum(call_in_ts[i]))
 call_in = np.array(time_nums)
-print call_in
 
 # get call duration
 call_enter_ts = np.genfromtxt(data_name, dtype=str, delimiter = ",", skip_header=1, usecols=7)
@@ -94,7 +92,6 @@ for i in range(0,call_enter_ts.size):
     time_nums.append(tsToNum(call_enter_ts[i]))
 call_done = np.array(time_nums)
 call_dur = np.subtract(call_done,call_in)
-print call_dur
 
 
 # dispatch times
@@ -103,17 +100,16 @@ d_time = []
 for i in range(0,disp_ts.size):
     if(disp_ts[i]==''):
         d_time.append(0)
-        bad_ind.append(i)
+       # bad_ind.append(i)
     else:d_time.append(tsToNum(disp_ts[i]))
 d_time = np.array(d_time)
 # on scene times
 os_ts = np.genfromtxt(data_name, dtype=str, delimiter = ",", skip_header=1, usecols=10)
-print os_ts
 os_time = []
 for i in range(0,os_ts.size):
     if(os_ts[i]==''):
         os_time.append(0)
-        bad_ind.append(i)
+        #bad_ind.append(i)
     else: os_time.append(tsToNum(os_ts[i]))
 os_time = np.array(os_time)
 resp_time = np.subtract(os_time,d_time)
@@ -131,15 +127,36 @@ data = np.delete(data,bad_ind)
 
 
 #this will sort data by call number, not sure if this is needed
-# sort = np.sort(data,axis=0,order=['callNum'])
-# print sort
+data = np.sort(data,axis=0,order=['callNum'])
 
+dup_ind=[]
+for i in range(0,data.shape[0]):
+    r_times = []
+    r_times.append(data[i][6])
+    call = data[i][0]
+    while i+1<data.shape[0] and data[i+len(r_times)][0]==call:
+        dup_ind.append(i+len(r_times))
+        r_times.append(data[i+len(r_times)][6])
+    for j in range(0,len(r_times)):
+        if r_times[j]<0:
+            r_times[j]=0
+    avg_res = 0;
+    if np.sum(r_times)>0:
+        while np.isin(0,r_times):
+            r_times.remove(0)
+        avg_res=np.average(r_times)
+    data[i][6] = avg_res
+
+dup_ind = np.unique(dup_ind)
+data = np.delete(data,dup_ind)
 
 # write cleaned data to a csv file (so that we only have to clean once)
 # csv out file will be filename_clean.csv
 data_out_name = data_name[:-4]+"_clean.csv"
 w_data = pd.DataFrame(data)
-w_data.to_csv(data_out_name,header=None, index=None)
+w_data.to_csv(data_out_name, index=None)
+
+print "done! :)"
 
 
 
